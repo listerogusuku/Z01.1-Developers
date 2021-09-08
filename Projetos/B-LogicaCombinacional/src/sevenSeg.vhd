f858f3fbecacfad55b1297495f4b5d93414baf96
Leticiacb1 <leticiacb1@al.insper.edu.br>
2021-09-08 14:55:28 -0300

    funcionando e testado

--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -10,4 +10,16 @@ end entity;
 architecture arch of sevenSeg is
 begin
 
+	leds<= "0000001" when bcd = "0000" else 
+	"0110000" when bcd = "0001" else 
+	"0010010" when bcd = "0010" else 
+	"0000110" when bcd = "0011" else 
+	"1001011" when bcd = "0100" else 
+	"0100100" when bcd = "0101" else 
+	"0100000" when bcd = "0110" else 
+	"0001111" when bcd = "0111" else 
+	"0000000" when bcd = "1000" else 
+	"0001100" when bcd = "1001" else 
+	"1111111";
+
 end architecture;
