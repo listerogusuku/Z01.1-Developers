eaf86c0883f28c95827664c648d627d1ac629a64
Adneycm <adneycm@al.insper.edu.br>
2021-09-22 17:55:55 -0300

    Ram64 - Adney

--- Projetos/D-LogicaSequencial/src/Ram64.vhd
+++ Projetos/D-LogicaSequencial/src/Ram64.vhd
@@ -60,5 +60,99 @@ architecture arch of Ram64 is
 
 begin
 
+		--seleciona multiplos loads
+		dmux: DMux8Way port map ( 
+			a => load,
+			sel => address(2 downto 0),
+			q0 => load0,
+			q1 => load1,
+			q2 => load2,
+			q3 => load3,
+			q4 => load4,
+			q5 => load5,
+			q6 => load6,
+			q7 => load7
+			);
+		
+		
+			--Controlam saídas e entradas de loads
+			mux: Mux8Way16 port map ( --seleciona saidas
+			a =>output0,
+			b =>output1,
+			c =>output2,
+			d =>output3,
+			e =>output4,
+			f =>output5,
+			g =>output6,
+			h =>output7,
+			sel => address(2 downto 0),
+			q=>output 
+			);
+
+	Ram8_0 : Ram8 port map (
+		clock => clock,
+		input => input,
+		load => load0,
+		address => address(2 downto 0),
+		output => output0
+		);
+
+	Ram8_1 : Ram8 port map (
+		clock => clock,
+		input => input,
+		load => load1,
+		address => address(2 downto 0),
+		output => output1
+		);
+
+	Ram8_2 : Ram8 port map (
+		clock => clock,
+		input => input,
+		load => load2,
+		address => address(2 downto 0),
+		output => output2
+		);
+
+	Ram8_3 : Ram8 port map (
+		clock => clock,
+		input => input,
+		load => load3,
+		address => address(2 downto 0),
+		output => output3
+		);
+
+	Ram8_4 : Ram8 port map (
+		clock => clock,
+		input => input,
+		load => load4,
+		address => address(2 downto 0),
+		output => output4
+		);
+
+	Ram8_5 : Ram8 port map (
+		clock => clock,
+		input => input,
+		load => load5,
+		address => address(2 downto 0),
+		output => output5
+		);
+
+	Ram8_6 : Ram8 port map (
+		clock => clock,
+		input => input,
+		load => load6,
+		address => address(2 downto 0),
+		output => output6
+		);
+
+	Ram8_7 : Ram8 port map (
+		clock => clock,
+		input => input,
+		load => load7,
+		address => address(2 downto 0),
+		output => output7
+		);
+
+
 
 end architecture;
